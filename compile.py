c3056ab58ddf4ff821e6bde77ab7c08e73d2eb41
Nick Dyer <nickedyer@gmail.com>
2021-05-30 00:15:59 -0400

    added compiling script, added pyinstaller to requirements.txt

---
+++ compile.py
@@ -0,0 +1,15 @@
+import os
+import PyInstaller.__main__
+import pocket_friends
+
+script_dir = os.path.dirname(__file__)
+
+PyInstaller.__main__.run([
+    '{0}/pocket_friends/__main__.py'.format(script_dir),
+    '--clean',
+    '--noconsole',
+    '--onefile',
+    '--name=pocket_friends-{0}'.format(pocket_friends.__version__),
+    '--collect-all=pocket_friends',
+    '--icon={0}/pocket_friends/game_files/resources/images/icon/icon.ico'.format(script_dir)
+])
